f674010b22fbaf05f20ef3fd29db72fb560f50a7
polinabeskletko-star <polina.beskletko@gmail.com>
2026-01-13 20:06:47 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,7 +80,7 @@ except ValueError:
     MAXIM_ID = 0
 
 # Теннисный код и дата действия
-TENNIS_ACCESS_CODE = "33836555#"
+TENNIS_ACCESS_CODE = "31806567#"
 TENNIS_CODE_VALID_UNTIL = "12 апреля 2026"
 
 # ========== ДАТАКЛАССЫ ==========
